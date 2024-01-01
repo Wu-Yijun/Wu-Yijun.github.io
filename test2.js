ed4b6723764b2b4f515c4757a7d11d7acbd53b2a
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-01-01 12:28:09 +0800

    try show highlights

--- test2.js
+++ test2.js
@@ -1,5 +1,5 @@
-import hljs from './highlight.js/es/core.js';
-import javascript from './highlight.js/es/languages/javascript.js';
+import hljs from './node_modules/highlight.js/es/core.js';
+import javascript from './node_modules/highlight.js/es/languages/javascript.js';
 
 hljs.registerLanguage('javascript', javascript);
 
