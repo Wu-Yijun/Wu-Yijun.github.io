9bf65ba718043e9016c44a19d25476f389942fce
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2024-01-01 19:32:15 +0800

    code block done

---
+++ assets/test_code.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+/***
+ * @author 匿名（中文文本测试）
+ * @date 2024-1-1
+ ***/
+using namespace std;
+
+#define STR1(x) "He" #x #x "o"
+constexpr char kStr2[] = "world";
+struct Sign {
+    char sign;
+    const int nextline;
+    static const bool yes = true;
+    vector<int> a;
+    int operator()() const { return 55 + yes; }
+};
+Sign s{sign : '!', nextline : '\n', a : {0b11, 0x22, 033, 44L, 55ULL}};
+
+inline constexpr int fun() noexcept { return (1 << 2); }
+
+int main() {
+    if (kStr2[1] == 'e' && s.yes)
+        printf("%s %s%c%c", STR1(l) ",", kStr2, (&s)->sign, s.nextline);
+    else
+        std::cout << s.a.front() << fun() << s() << endl;
+    return 0;
+}
